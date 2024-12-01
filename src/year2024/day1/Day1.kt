f42da8c72849268aa7ed93f01ec6ab26d5f9dfe9
Michael de Kaste <michael.de.kaste@topicus.nl>
2024-12-01 06:16:38 +0100

    cleaned up day 1

--- src/year2024/day1/Day1.kt
+++ src/year2024/day1/Day1.kt
@@ -9,11 +9,18 @@ fun main(){
 }
 
 class Day1 : Challenge(){
-    val parsed = input.lines().map { it.split("   ").map(String::toInt).let { (a, b) -> a to b } }.unzip()
+    val left: List<Int>
+    val right: List<Int>
+    init {
+        input.lines()
+            .map { it.split("   ").map(String::toInt).let { (a, b) -> a to b } }
+            .unzip()
+            .also { (l, r) -> left = l.sorted(); right = r.sorted() }
+    }
 
-    override fun part1() = parsed.first.sorted().zip(parsed.second.sorted()).sumOf { abs(it.first - it.second) }
+    override fun part1() = left.zip(right, Int::minus).sumOf(::abs)
 
-    override fun part2() = with(parsed.second.groupingBy { it }.eachCount()){
-        parsed.first.sumOf { it * getOrDefault(it, 0) }
+    override fun part2() = with(right.groupingBy { it }.eachCount()){
+       left.sumOf { it * getOrDefault(it, 0) }
     }
 }
