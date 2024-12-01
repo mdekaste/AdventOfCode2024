e55be6c09470621ec7852069d5c3b9bb2bfc6408
Michael de Kaste <michael.de.kaste@topicus.nl>
2024-12-01 06:10:10 +0100

    Day1!

--- src/year2024/day1/Day1.kt
+++ src/year2024/day1/Day1.kt
@@ -1,18 +1,19 @@
 package year2024.day1
 
 import core.Challenge
+import kotlin.math.abs
 
 fun main(){
-    Day1().solve()
+    Day1().part1().let(::println)
+    Day1().part2().let(::println)
 }
 
 class Day1 : Challenge(){
-    override fun part1(): Any? {
-        TODO("Not yet implemented")
-    }
+    val parsed = input.lines().map { it.split("   ").map(String::toInt).let { (a, b) -> a to b } }.unzip()
 
-    override fun part2(): Any? {
-        TODO("Not yet implemented")
-    }
+    override fun part1() = parsed.first.sorted().zip(parsed.second.sorted()).sumOf { abs(it.first - it.second) }
 
+    override fun part2() = with(parsed.second.groupingBy { it }.eachCount()){
+        parsed.first.sumOf { it * getOrDefault(it, 0) }
+    }
 }
