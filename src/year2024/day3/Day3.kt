a084ba6b503e0d58d3ee9ed6ebed278aeb530a10
Michael de Kaste <michael.de.kaste@topicus.nl>
2024-12-02 21:30:44 +0100

    Day 3 prep

---
+++ src/year2024/day3/Day3.kt
@@ -0,0 +1,19 @@
+package year2024.day3
+
+import core.AdventOfCode
+
+fun main(){
+    val day3 = Day3()
+    day3.part1()
+    day3.part2()
+}
+
+class Day3 : AdventOfCode({
+    val parsed = input
+    part1 {
+        parsed
+    }
+    part2 {
+        parsed
+    }
+})
