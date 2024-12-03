42e45c2d1cdb6bfc30674a31a35981db1583c6a6
Michael de Kaste <michael.de.kaste@topicus.nl>
2024-12-03 08:44:37 +0100

    cleaner day 3

--- src/core/inputParsing/String.kt
+++ src/core/inputParsing/String.kt
@@ -2,13 +2,13 @@ package core.inputParsing
 
 fun String.extractInts(): List<Int> = Regex("""-?\d+""").findAll(this).map { it.value.toInt() }.toList()
 
-operator fun MatchResult.component1() = groupValues[1]
-operator fun MatchResult.component2() = groupValues[2]
-operator fun MatchResult.component3() = groupValues[3]
-operator fun MatchResult.component4() = groupValues[4]
-operator fun MatchResult.component5() = groupValues[5]
-operator fun MatchResult.component6() = groupValues[6]
-operator fun MatchResult.component7() = groupValues[7]
-operator fun MatchResult.component8() = groupValues[8]
-operator fun MatchResult.component9() = groupValues[9]
-operator fun MatchResult.component10() = groupValues[10]
+operator fun MatchResult.component1() = groupValues.getOrNull(1)
+operator fun MatchResult.component2() = groupValues.getOrNull(2)
+operator fun MatchResult.component3() = groupValues.getOrNull(3)
+operator fun MatchResult.component4() = groupValues.getOrNull(4)
+operator fun MatchResult.component5() = groupValues.getOrNull(5)
+operator fun MatchResult.component6() = groupValues.getOrNull(6)
+operator fun MatchResult.component7() = groupValues.getOrNull(7)
+operator fun MatchResult.component8() = groupValues.getOrNull(8)
+operator fun MatchResult.component9() = groupValues.getOrNull(9)
+operator fun MatchResult.component10() = groupValues.getOrNull(10)
--- src/year2024/day3/Day3.kt
+++ src/year2024/day3/Day3.kt
@@ -1,31 +1,41 @@
 package year2024.day3
 
 import core.AdventOfCode
-import core.inputParsing.component1
-import core.withNoNulls
 
 fun main(){
     val day3 = Day3()
     day3.part1()
     day3.part2()
 }
-
 class Day3 : AdventOfCode({
     val regex = """(mul\((\d+),(\d+)\)|do\(\)|don't\(\))""".toRegex()
-    val parsed = regex.findAll(input)
-        .map(MatchResult::destructured)
-        .map { (inst, a, b) -> inst to (withNoNulls(a.toIntOrNull(), b.toIntOrNull(), Int::times) ?: 0) }
+    val parsed = regex.findAll(input).map(Instruction::of)
 
     part1 {
-        parsed.sumOf { it.second }
+        parsed.filterIsInstance<Instruction.MUL>().sumOf(Instruction.MUL::value)
     }
     part2 {
-       parsed.fold(true to 0) { (canDo, acc), (instruction, mul) ->
-           when(instruction){
-               "do()" -> true to acc
-               "don't()" -> false to acc
-               else -> if(canDo) true to (acc + mul) else false to acc
-           }
-       }.second
+        parsed.fold(true to 0){ (doIt, acc), inst ->
+            when(inst){
+                Instruction.DO -> true to acc
+                Instruction.DONT -> false to acc
+                is Instruction.MUL -> if (doIt) true to acc + inst.value else false to acc
+            }
+        }.second
     }
 })
+sealed interface Instruction {
+    data object DO : Instruction
+    data object DONT : Instruction
+    data class MUL(val value: Int) : Instruction
+    companion object {
+        fun of(matchResult: MatchResult): Instruction {
+            val (inst, a, b) = matchResult.destructured
+            return when(inst){
+                "do()" -> DO
+                "don't()" -> DONT
+                else -> MUL(a.toInt() * b.toInt())
+            }
+        }
+    }
+}
