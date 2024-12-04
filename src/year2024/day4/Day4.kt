85368cd7f68159bbfdecec5f16601b84326ca04e
Michael de Kaste <michael.de.kaste@topicus.nl>
2024-12-04 07:14:35 +0100

    Day4 cleanup 2

--- src/year2024/day4/Day4.kt
+++ src/year2024/day4/Day4.kt
@@ -1,39 +1,30 @@
 package year2024.day4
 
 import core.AdventOfCode
-import core.inputParsing.extractInts
-import core.interval.Interval
 import core.toGraph
 import core.twoDimensional.*
 
 fun main(){
     val day4 = Day4()
-    day4.part1()
-    day4.part2()
+    day4.solve(100)
 }
 
 
-
+fun Grid<*>.grow(o1: Point, dir: Point, amount: Int): String = List(amount){ get(o1 + dir * it) }.joinToString("")
 class Day4 : AdventOfCode({
     val parsed = input.lines().toGraph()
 
-    fun Grid<String>.grow(origin: Point, amount: Int, vararg directions: Point): List<String> = buildList {
-        for(direction in directions) {
-            add(generateSequence(origin, direction::plus).take(amount).map(this@grow::get).joinToString(""))
-        }
-    }
-
     part1{
         parsed.keys.sumOf { key ->
-            parsed.grow(key, 4, *WINDS).count("XMAS"::equals)
+            WINDS.count { wind -> parsed.grow(key, wind, 4) == "XMAS" }
         }
     }
 
 
     part2{
         parsed.keys.count { key ->
-            parsed.grow(key + NORTH_WEST, 3, SOUTH_EAST).any { it in listOf("SAM", "MAS") } &&
-            parsed.grow(key + NORTH_EAST, 3, SOUTH_WEST).any { it in listOf("SAM", "MAS") }
+            parsed.grow(key + NORTH_WEST, SOUTH_EAST, 3) in listOf("SAM", "MAS") &&
+            parsed.grow(key + NORTH_EAST, SOUTH_WEST, 3) in listOf("SAM", "MAS")
         }
     }
 })
