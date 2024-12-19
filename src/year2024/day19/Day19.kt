5e2e6f5dba8ae835441cb4750a96db71a4166f09
Michael de Kaste <m.dekaste@hotmail.com>
2024-12-19 08:51:08 +0100

    goodbye prefixmap (fast but short = better ofc ofc)

--- src/year2024/day19/Day19.kt
+++ src/year2024/day19/Day19.kt
@@ -4,52 +4,26 @@ import core.AdventOfCode
 import core.inputParsing.splitOnEmptyLine
 
 fun main(){
-    repeat(10000){
-        val day19 = Day19()
-        day19.solve()
-    }
     val day19 = Day19()
     day19.solve()
 
 }
 
 class Day19 : AdventOfCode({
-    val (towels, designs) = input.splitOnEmptyLine().let { (a,b) -> a.split(", ") to b.lines() }
-
-    class PrefixMap : MutableMap<Char, PrefixMap> by mutableMapOf(){
-        var isEnd = false
-        fun add(prefix: String): Unit = when(prefix){
-            "" -> isEnd = true
-            else -> getOrPut(prefix[0], :: PrefixMap).add(prefix.substring(1))
-        }
-    }
-
-    val prefixMap = PrefixMap().apply {
-        for (towel in towels) {
-            add(towel)
-        }
-    }
+    val (towels, designs) = input.splitOnEmptyLine().let { (a,b) -> a.split(", ").toSet() to b.lines() }
 
     val memoization = mutableMapOf("" to 1L)
-    fun countPossible(design: String): Long = memoization.getOrPut(design){
-        var prefixMap: PrefixMap = prefixMap
-        var count = 0L
-        for((index, character) in design.withIndex()){
-            prefixMap = prefixMap[character] ?: break
-            if(prefixMap.isEnd){
-                count += countPossible(design.substring(index + 1))
-            }
-        }
-        count
+    fun possibleWays(design: String): Long = memoization.getOrPut(design){
+        design.indices
+            .filter { design.take(it + 1) in towels }
+            .sumOf { possibleWays(design.drop(it + 1)) }
     }
 
-    val possibleDesigns = designs.map(::countPossible)
-
     part1{
-        possibleDesigns.count { it > 0 }
+        designs.count { possibleWays(it) > 0 }
     }
 
     part2{
-        possibleDesigns.sum()
+        designs.sumOf { possibleWays(it) }
     }
 })
