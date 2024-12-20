69ccb0e2cfa265578f10ab1a483557c86dd48fde
Michael de Kaste <m.dekaste@hotmail.com>
2024-12-20 10:50:26 +0100

    smoll

--- src/year2024/day20/Day20.kt
+++ src/year2024/day20/Day20.kt
@@ -3,31 +3,26 @@ package year2024.day20
 import core.AdventOfCode
 import core.inputParsing.toGrid
 import core.twoDimensional.*
+import java.util.LinkedList
 
 fun main(){
     val day20 = Day20()
-    day20.solve(1000)
+    day20.solve(100)
 }
 
+typealias Step = Pair<Point?, Point>
 class Day20 : AdventOfCode({
-    val path = buildList {
-        val parsed = input.toGrid()
-        var prev: Point? = null
-        var cur: Point = parsed.entries.first { it.value == 'S' }.key
-        while(add(cur)){
-            val next = cur.cardinals().firstOrNull { parsed[it] != '#' && it != prev } ?: break
-            prev = cur
-            cur = next
-        }
-    }
+    val grid = input.toGrid()
+    val start = grid.entries.first { it.value == 'S' }.key
+
+    fun Step.next(): Step? = second.cardinals().firstOrNull { grid[it] != '#' && it != first }?.let { second to it }
+    val path = generateSequence(null to start, Step::next).mapTo(mutableListOf(), Step::second)
 
     fun solve(cheatTime: Int, saveTime: Int) = path.withIndex().sumOf { (index, point) ->
         path.subList(index, path.size).withIndex().count { (oIndex, oPoint) ->
-            val distance = point.manhattanDistance(oPoint)
-            distance in 2..cheatTime && oIndex - distance >= saveTime
+            point.manhattanDistance(oPoint).let { it in 2..cheatTime && oIndex - it >= saveTime }
         }
     }
-
 
     part1{
         solve(2, 100)
