f6439a96ce5adba3d0c437b9fb19ef6d4857f1c9
Michael de Kaste <m.dekaste@hotmail.com>
2025-12-02 07:07:19 +0100

    Day 2 - regex

--- src/year2025/day2/Day2.kt
+++ src/year2025/day2/Day2.kt
@@ -7,49 +7,22 @@ fun main(){
     Day2.part2()
 }
 
+
+
 object Day2 : AdventOfCode({
-    val parsed = input.split(",").map { it.split("-").let { (x, y) -> x.toLong()..y.toLong()} }
+    val parsed = input
+        .split(",")
+        .map { it.split("-").let { (a,b) -> a.toLong()..b.toLong() } }
 
-    fun String.isRepeated2(): Boolean {
-        for(i in length / 2 downTo 1){
-            if(chunked(i).toSet().size == 1){
-                return true
-            }
-        }
-        return false
-    }
-
-    fun String.isRepeated(): Boolean {
-        if(length % 2 == 0){
-            return substring(0, length / 2) == substring(length / 2)
-        }
-        return false
-    }
-
-    fun invalidId(range: LongRange): List<Long> {
-        return buildList {
-            for(i in range){
-                if(i.toString().isRepeated()){
-                    add(i)
-                }
-            }
-        }
-    }
+    fun solve(regex: Regex) = parsed.flatMap { range ->
+        range.filter { number -> number.toString().matches(regex) }
+    }.sum()
 
     part1{
-        parsed.flatMap { invalidId(it) }.sum()
+        solve("""(\d+)\1""".toRegex())
     }
 
-    fun invalidId2(range: LongRange): List<Long> {
-        return buildList {
-            for(i in range){
-                if(i.toString().isRepeated2()){
-                    add(i)
-                }
-            }
-        }
-    }
     part2{
-        parsed.flatMap { invalidId2(it) }.sum()
+        solve( """(\d+)\1+""".toRegex())
     }
 })
