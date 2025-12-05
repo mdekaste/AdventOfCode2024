2f3af7898bf32b9a1ce4f83de59e7786b48c77a5
michael-de-kaste <m.dekaste@hotmail.com>
2025-12-05 13:52:50 +0100

    Day 5 cleanup

--- src/year2025/day5/Day5.kt
+++ src/year2025/day5/Day5.kt
@@ -6,29 +6,29 @@ import java.util.TreeMap
 import kotlin.collections.component1
 import kotlin.collections.component2
 import kotlin.collections.set
+import kotlin.text.split
 
 fun main(){
-    Day5.part1()
-    Day5.part2()
+    Day5.solve()
 }
 
 object Day5 : AdventOfCode({
-    val map = TreeMap<Long, Boolean>()
+    val map = TreeMap<Long, Boolean>(mapOf(Long.MAX_VALUE to false))
     val ingredients = input
-        .splitOnEmptyLine()
-        .let { (a, b) ->
-            a.lines().forEach {
-                val (left, right) = it.split("-").map(String::toLong)
-                val fromBoundValue = map.ceilingEntry(left)?.value ?: false
-                val untilBoundValue = map.higherEntry(right + 1)?.value ?: false
+        .split(System.lineSeparator() + System.lineSeparator())
+        .let { (freshRange, ingredients) ->
+            freshRange.lines().forEach { freshLine ->
+                val (left, right) = freshLine.split("-").map(String::toLong)
+                val fromBoundValue = map.ceilingEntry(left).value
                 map.subMap(left, true, right + 1, true).clear()
                 if (!fromBoundValue) map[left] = false
-                if (!untilBoundValue) map[right + 1] = true
+                map[right + 1] = true
             }
-            b.lines().map(String::toLong)
+            ingredients.lines().map(String::toLong)
         }
+
     part1{
-        ingredients.count{ map.ceilingEntry(it)?.value ?: false }
+        ingredients.count{ map.ceilingEntry(it).value }
     }
     part2{
         map.entries.zipWithNext { (l1, _), (l2, b2) -> if(b2) l2 - l1 else 0 }.sum()
