62c44d3794dc0977174f519fe6add55a36120199
Michael de Kaste <m.dekaste@hotmail.com>
2025-12-06 07:28:49 +0100

    Day6

---
+++ src/year2025/day6/Day6.kt
@@ -0,0 +1,44 @@
+package year2025.day6
+
+import core.AdventOfCode
+import core.inputParsing.splitOnEmptyLine
+import core.unzip
+import javax.swing.Spring.height
+
+fun main() {
+    Day6.part1()
+    Day6.part2()
+}
+
+object Day6 : AdventOfCode({
+    part1 {
+        input.lines()
+            .map { it.split("""\s+""".toRegex()) }
+            .unzip()
+            .sumOf { (a, b, c, d, op) ->
+                when (op) {
+                    "+" -> a.toLong() + b.toLong() + c.toLong() + d.toLong()
+                    else -> a.toLong() * b.toLong() * c.toLong() * d.toLong()
+                }
+            }
+    }
+
+    fun List<String>.flipDiagonal(): List<String> = first().indices.map { x ->
+        indices.map { y -> get(y)[x] }.joinToString("").trim()
+    }
+
+    data class Aggregate(val sum: Long, val agg: Long, val op: Char) {
+        fun submit(line: String): Aggregate = when {
+            line.isEmpty() -> copy(sum = sum + agg, agg = 0)
+            !line.last().isDigit() -> copy(agg = line.dropLast(1).trim().toLong(), op = line.last())
+            else -> copy(agg = if (op == '*') agg * line.toLong() else agg + line.toLong())
+        }
+    }
+
+    part2 {
+        input.lines()
+            .flipDiagonal()
+            .fold(Aggregate(0, 0, '-'), Aggregate::submit)
+            .let { it.sum + it.agg }
+    }
+})
